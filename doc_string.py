67ceff345ea8c34e57469550168df9f3c5c1ab60
Dillon Heiden <dillonheiden@Dillons-MacBook-Pro.local>
2022-01-24 18:20:16 +0000

    testing commit

---
+++ doc_string.py
@@ -0,0 +1,12 @@
+def trade_exposure (x,y,z):
+    
+    """
+    returns x + y + z
+    :param x: int.
+    :param y: int.
+    :param z: int.
+    :return: int sum of x , y and z.
+    """
+    return x + y + z
+
+print(trade_exposure(2,2,2))
